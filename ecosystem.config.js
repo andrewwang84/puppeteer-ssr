3fd84b80e101c9612b434c56fdb1102364e92926
andrewwang <andrewwang@talk2yam.com>
2019-01-16 15:47:01 +0800

    config fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps : [{
     name: "ssr-yam",
-    script: "/home/andrew_wang/projects/puppeteer/webroot/bin/www",
+    script: "/home/www/ssr.yam.com/webroot/bin/www",
     exec_mode: "cluster",
     instance: 1,
     max_memory_restart: "150M",
-    out_file: "/home/andrew_wang/projects/puppeteer/logs/access.log",
-    error_file: "/home/andrew_wang/projects/puppeteer/logs/error.log",
+    out_file: "/home/www/ssr.yam.com/logs/access.log",
+    error_file: "/home/www/ssr.yam.com/logs/error.log",
     log_date_format: "YYYY-MM-DD HH:mm:ss",
     restart_delay: 5000,
     env: {
